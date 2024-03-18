bea3c1852f682adb27caf699ca84d7b535872d8d
kacemabidi12 <kabidi@mail.roanoke.edu>
2024-03-18 01:51:41 -0400

    Kacem did some changes

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 function Kacem(){
   return(
-    <text>This is Kacem's component !</text>
+    <text>This is Kacem's component!</text>
   )
 }
 
